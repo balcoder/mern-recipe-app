213aeea5255fcb2ba11b29454850f025dcc2c0e1
balcoder <des.barrett@gmail.com>
2025-04-16 14:21:08 +0100

    created test api route part 2

---
+++ api/controllers/user.controller.js
@@ -0,0 +1,5 @@
+export const test = (req, res) => {
+    res.json({
+        message: 'Hello World! from controllers/user.controller.js is Foo'
+    })
+}
--- api/index.js
+++ api/index.js
@@ -1,23 +1,22 @@
 import 'dotenv/config';
 import express from 'express';
 import mongoose from 'mongoose';
+import userRouter from './routes/user.route.js';
 
 mongoose.connect(process.env.MONGO_URI)
    .then(() => {
-      console.log('Connected to MongoDB')
+      console.log('Connected to MongoDB');
    })
    .catch((err) => {
       console.log(err);
-   })
+   });
 
 
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
 
- app.get('/', (req, res) => {
-    res.send('Hello World!')
- })
+ app.use("/api/user", userRouter);
 
  app.listen(port, ()=> {
-    console.log(`Express Server running on port ${port}!!!`)
- })
+    console.log(`Express Server running on port ${port}!!!`);
+ });
