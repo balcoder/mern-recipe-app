c3a997dd303076cbb6042a673135b5d028f104e8
balcoder <des.barrett@gmail.com>
2025-04-17 15:32:06 +0100

    created middleware & function to handle errors

--- api/index.js
+++ api/index.js
@@ -22,6 +22,17 @@ const port = 3000;
  app.use("/api/user", userRouter);
  app.use('/api/auth', authRouter);
 
+// middleware for handling errors
+app.use((err, req, res, next) => {
+   const statusCode = err.statusCode || 500;
+   const message = err.message || 'Internal Server Error';   
+   return res.status(statusCode).json({
+      success: false,
+      statusCode,
+      message
+   })
+ });
+
  app.listen(port, ()=> {
     console.log(`Express Server running on port ${port}!!!`);
  });
